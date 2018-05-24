0c6349533b99bbb122b4bd5fd8772c477ac38db2
bingoogolapple <bingoogolapple@gmail.com>
2018-05-24 13:15:18 +0800

    DSL

--- groovy/src/main/groovy/cn/bingoogolapple/dsl/groovy/17_dsl.groovy
+++ groovy/src/main/groovy/cn/bingoogolapple/dsl/groovy/17_dsl.groovy
@@ -87,4 +87,145 @@ def florist = Florist.create {
     }
 }
 
-println florist
+//println florist
+
+interface Node {
+    String render()
+}
+
+class Content implements Node {
+    String text
+
+    Content(String text) {
+        this.text = text
+    }
+
+    @Override
+    String render() {
+        return text
+    }
+}
+
+class Tag implements Node {
+    List<Node> children = []
+    Map<String, String> properties = [:]
+    String name
+    Integer level = 0
+
+    Tag(String name, Integer level) {
+        this.name = name
+        this.level = level
+    }
+
+    @Override
+    String render() {
+        StringBuilder result = new StringBuilder()
+        level.times {
+            result.append('    ')
+        }
+
+        result.append("<$name").append()
+        if (!properties.isEmpty()) {
+            properties.each { key, value ->
+                result.append(" $key=\"$value\"")
+            }
+        }
+        result.append('>')
+
+        if (!children.isEmpty() && children.size() == 1 && children.get(0) instanceof Content) {
+            result.append(children.get(0).render())
+        } else if (!children.isEmpty()) {
+            children.each {
+                result.append('\n').append(it.render())
+            }
+            result.append('\n')
+
+            level.times {
+                result.append('    ')
+            }
+        }
+        result.append("</$name>")
+        return result.toString()
+    }
+}
+
+class TagDsl {
+    Tag tag
+
+    TagDsl(Tag tag) {
+        this.tag = tag
+    }
+
+    static Tag make(Closure closure) {
+        TagDsl tagDsl = new TagDsl()
+        closure.resolveStrategy = Closure.DELEGATE_FIRST
+        closure.delegate = tagDsl
+        closure.call()
+        return tagDsl.tag
+    }
+
+    def methodMissing(String name, Object args) {
+        if (args[0] instanceof Closure) {
+            Tag param
+            if (tag == null) {
+                tag = new Tag(name, 0)
+                param = tag
+            } else {
+                Tag child = new Tag(name, tag.level + 1)
+                tag.children.add(child)
+                param = child
+            }
+            Closure closure = args[0] as Closure
+            closure.resolveStrategy = Closure.DELEGATE_FIRST
+            closure.delegate = new TagDsl(param)
+            closure.call()
+        } else if (args[0] instanceof String) {
+            tag.properties.put(name, args[0])
+        } else {
+            return "the method ${name} is missing, the params is ${args}"
+        }
+    }
+
+    @Override
+    Object getProperty(String propertyName) {
+        if (propertyName == 'tag') {
+            return tag
+        } else {
+            Node content = new Content(propertyName)
+            tag.children.add(content)
+            return content
+        }
+    }
+}
+
+println TagDsl.make {
+    html {
+        head {
+            meta {
+                charset 'utf-8'
+            }
+            title {
+                BGA
+            }
+        }
+        body {
+            style 'position: static; overflow: visible;'
+            ul {
+                id 'root'
+                'class' 'test'
+                li {
+                    a {
+                        href 'https://github.com/bingoogolapple'
+                        GitHub
+                    }
+                }
+                li {
+                    a {
+                        href 'http://www.bingoogolapple.cn'
+                        个人博客
+                    }
+                }
+            }
+        }
+    }
+}.render()
