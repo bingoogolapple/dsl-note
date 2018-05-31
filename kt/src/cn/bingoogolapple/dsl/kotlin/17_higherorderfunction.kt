97756d9d8d3cd0abc3329efcb515dce055bf7e2e
bingoogolapple <bingoogolapple@gmail.com>
2018-05-31 09:28:13 +0800

    高阶函数

--- kt/src/cn/bingoogolapple/dsl/kotlin/17_higherorderfunction.kt
+++ kt/src/cn/bingoogolapple/dsl/kotlin/17_higherorderfunction.kt
@@ -1,5 +1,8 @@
 package cn.bingoogolapple.dsl.kotlin
 
+import java.io.BufferedReader
+import java.io.FileReader
+
 fun main(args: Array<String>) {
     // 包级函数
     args.forEach(::println)
@@ -13,7 +16,8 @@ fun main(args: Array<String>) {
     // 调用者引用函数
     args.forEach(pdfPrinter::println)
 
-    test1()
+//    test1()
+    test2()
 }
 
 class PdfPrinter {
@@ -34,7 +38,7 @@ fun test1() {
     val sum = list.flatMap { it }.reduce { acc, intElement -> acc + intElement }
     println(sum)
 
-    // 指定初始值为 StringBuffer 的实例对象
+    // fold 有初始值的 reduce。指定初始值为 StringBuffer 的实例对象
     println((0..6).map(::factorial).fold(StringBuffer()) { acc, i ->
         acc.append(i).append(",")
     })
@@ -45,4 +49,54 @@ fun factorial(n: Int): Int {
         0 -> 1
         else -> (1..n).reduce { acc, i -> acc * i }
     }
+}
+
+fun test2() {
+    val person = findPerson()
+    person?.let {
+        it.work()
+        // 这里不用再加 ? 了
+        person.work()
+    }
+    person?.let { (name, age) ->
+        println(name)
+        println(age)
+    }
+    person?.apply {
+        work()
+        println(name)
+        println(age)
+    }
+
+
+    val br = BufferedReader(FileReader("settings.gradle"))
+    println(br.readText())
+
+    with(br) {
+        var line: String?
+        while (true) {
+            line = readLine() ?: break
+            println(line)
+        }
+        close()
+    }
+
+    br.use {
+        var line: String?
+        while (true) {
+            line = it.readLine() ?: break
+            println(line)
+        }
+        // 不用再调 close 方法，use 内部已经调了
+    }
+}
+
+data class Person(val name: String, val age: Int) {
+    fun work() {
+        println("$name is working!!!")
+    }
+}
+
+fun findPerson(): Person? {
+    return null
 }
